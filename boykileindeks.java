8fd7cfaa4afbb14add1ba093925d2b655d738dbc
EnisKaraman <98874248+EnisKaraman@users.noreply.github.com>
2022-02-08 11:59:42 +0300

    Add files via upload

---
+++ boykileindeks.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class boykileindeks {
+
+    public static void main(String[] args) {
+        Scanner klavye=new Scanner(System.in);
+
+        System.out.println("Boyunuzu Giriniz(metre cinsinden):");
+        float boy=klavye.nextFloat();
+
+        System.out.println("Kilonuzu Giriniz:");
+        double kilo=klavye.nextDouble();
+
+        double indeks=kilo/boy*boy;
+        System.out.println("Vicut İndeksiniz:"+indeks);
+    }
+}
