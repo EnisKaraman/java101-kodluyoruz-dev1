b912edcf877aa6accde95bf997bc1144ae696bca
EnisKaraman <98874248+EnisKaraman@users.noreply.github.com>
2022-03-11 10:33:19 +0300

    Add files via upload

---
+++ artıkyıl.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class artıkyıl {
+    
+    public static void main(String[] args) {
+        Scanner klavye=new Scanner(System.in);
+        
+         System.out.print("Yılı Giriniz:");
+         int yil=klavye.nextInt();
+          if (yil%4==0||yil%400==0) {
+              System.out.println(yil+" Nir Artık Yıldır!");   
+         }
+          else {
+              System.out.println(yil+" Bir Artık Yıl Değildir!");
+          }
+         
+     }
+    }
