ee2df58f3d857919acb8286015354c92bbc3b61c
EnisKaraman <98874248+EnisKaraman@users.noreply.github.com>
2022-06-29 12:55:26 +0300

    Update ekokebob.java

--- ekokebob.java
+++ ekokebob.java
@@ -2,33 +2,33 @@ import java.util.*;
 public class ekokebob {
     public static void main(String[] args) {
         Scanner klavye=new Scanner(System.in);
+        
+        int i=1;
+        int k=1;
+        int ebob=1;
+        
         System.out.println("1. Sayınızı Giriniz:");
         int s1=klavye.nextInt();
         System.out.println("2. Sayınızı Giriniz:");
         int s2=klavye.nextInt();
-        int i=1;
-        int k=1;
-        int ebob=1;
+
         while(i<s1){
             if (s1%i==0 && s2%i==0) {
                 System.out.println(i);
                 ebob=i;
-            }
-            
+            }           
             i++;
     }
         System.out.println(i);
         System.out.println("Ebob:"+ebob);
-
         
-        while(k<=(s1*s2)){
-            
+        while(k<=(s1*s2)){         
             if (k%s1==0 && k%s2==0) {
                 System.out.println("Ekok:"+k);
                 break;               
             }
             k++;            
         }
-            System.out.println("Ekok Sağlama:"+(s1*s2)/ebob);
+        System.out.println("Ekok Sağlama:"+(s1*s2)/ebob);
     }
 }
